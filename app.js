90d4678895697ccd9bbc98c8e6a559d4e8aa4b34
Soumyajit Biswas <55776513+WIKIJIT@users.noreply.github.com>
2023-01-14 10:22:17 +0530

    Changed from pm 2.5 to pm 10 and added calculation based on Indian metrics
    Changed from pm 2.5 to pm 10 and added calculation based on Indian metrics. Changed the code to set all the displayed values to null if place name entered does not exist. Also removed useless code and added PM 10 value, CO value and SO3 value for displaying.

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var desc = document.querySelector('.desc');
 var lat = document.querySelector('.lat');
 var long = document.querySelector('.long');
 var aqi = document.querySelector('.aqi');
+var pmI = document.querySelector('.pmI');
+var o3I = document.querySelector('.o3I');
 var hum = document.querySelector('.hum');
 var co = document.querySelector('.co');
 var feels = document.querySelector('.feels');
@@ -18,14 +20,14 @@ var longValue=null;
 var aqiValue=null;
 var coValue=null;
 var o3=null;
-var pm25=null;
+var pm10=null;
 var IHi = null;
 var Ilo = null;
 var BPHi = null;
 var BPlo = null;
 var Cp = null;
 var o3Index = null;
-var pm25Index = null;
+var pm10Index = null;
 var coIndex = null;
 var oValue;
 var cValue;
@@ -47,11 +49,11 @@ function a()
   feelsValue=parseInt(feelsValue-273);
   main.innerHTML = nameValue;
   desc.innerHTML = "Desc - "+descValue;
-  temp.innerHTML = "Temp - "+tempValue;
-  feels.innerHTML = "Feels Like - "+feelsValue;
+  temp.innerHTML = "Temp - "+tempValue+"°C";
+  feels.innerHTML = "Feels Like - "+feelsValue+"°C";
   hum.innerHTML = "Humidity - "+humValue+"%";
-  lat.innerHTML = "Latitute - "+latValue;
-  long.innerHTML = "Longitude - "+longValue;
+  lat.innerHTML = "Latitute - "+latValue+"°";
+  long.innerHTML = "Longitude - "+longValue+"°";
   b(latValue,longValue);
 })
 
@@ -63,208 +65,24 @@ function b(latValue,longValue)
   .then(responset => responset.json())
   .then(datat => {
     //var aqiValue = datat['list'][0]['main']['aqi'];
-    var coValue = datat['list'][0]['components']['co']*0.00087;
-    var o3Value = datat['list'][0]['components']['o3']*0.51;
-    var pm25Value = datat['list'][0]['components']['pm2_5'];
+    var coValue = datat['list'][0]['components']['co']*0.001;
+    var o3Value = datat['list'][0]['components']['o3'];
+    var pm10Value = datat['list'][0]['components']['pm10'];
     var cValue=coc(coValue);
     var oValue=ozone(o3Value);
-    var pmValue=pm(pm25Value);
-    //var aqiValue = calculateAQI(o3Value ,pm25Value, coValue);
+    var pmValue=pm(pm10Value);
+    var aqiValue = 0;
     aqiValue = Math.max(aqiValue,cValue);
     aqiValue = Math.max(aqiValue,oValue);
     aqiValue = Math.max(aqiValue,pmValue);
     aqi.innerHTML = "AQI - "+aqiValue;
-    co.innerHTML = "CO - "+coValue;
+    co.innerHTML = "CO - "+coValue+" mg/m3";
+    pmI.innerHTML = "PM10 - "+pmValue+" µg/m3";
+    o3I.innerHTML = "O3 - "+o3Value+" µg/m3";
     exit(0);
   })
 }
 
-// function calculateAQI(o3, pm25, co) {
-//   var aqiv = null;
-//   var IHi = null;
-//   var Ilo = null;
-//   var BPHi = null;
-//   var BPlo = null;
-//   var Cp = null;
-//   var o3Index = null;
-//   var pm25Index = null;
-//   var coIndex = null;
-//   // Ozone (O3) calculation
-  
-//   if (o3>=0 && o3<=50)
-//   {
-//     IHi=50;
-//     Ilo=0;
-//     BPHi=50;
-//     BPlo=0;
-//     Cp=o3;
-//     o3Index= ((IHi - Ilo) / (BPHi - BPlo))*(Cp - BPlo) + Ilo;
-//   }
-//   else if(o3>=51 && o3<=100)
-//   {
-//     IHi=100;
-//     Ilo=51;
-//     BPHi=100;
-//     BPlo=51;
-//     Cp=o3;
-//     o3Index= ((IHi - Ilo) / (BPHi - BPlo))*(Cp - BPlo) + Ilo;
-//   }
-//   else if(o3>=101 && o3<=168)
-//   {
-//     IHi=200;
-//     Ilo=101;
-//     BPHi=168;
-//     BPlo=101;
-//     Cp=o3;
-//     o3Index= ((IHi - Ilo) / (BPHi - BPlo))*(Cp - BPlo) + Ilo;
-//   }
-//   else if(o3>=169 && o3<=208)
-//   {
-//     IHi=300;
-//     Ilo=201;
-//     BPHi=208;
-//     BPlo=169;
-//     Cp=o3;
-//     o3Index= ((IHi - Ilo) / (BPHi - BPlo))*(Cp - BPlo) + Ilo;
-//   }
-//   else if(o3>=209 && o3<=748)
-//   {
-//     IHi=400;
-//     Ilo=301;
-//     BPHi=748;
-//     BPlo=209;
-//     Cp=o3;
-//     o3Index= ((IHi - Ilo) / (BPHi - BPlo))*(Cp - BPlo) + Ilo;
-//   }
-//   else
-//   {
-//     IHi=500;
-//     Ilo=401;
-//     BPHi=1000;
-//     BPlo=748;
-//     Cp=o3;
-//     o3Index= ((IHi - Ilo) / (BPHi - BPlo))*(Cp - BPlo) + Ilo;
-//   }
-//   aqiv = Math.max(aqiv, o3Index);
-
-//   // Particulate matter (PM2.5) calculation
-  
-//   if(pm25>=0 && pm<=30)
-//   {
-//     IHi=50;
-//     Ilo=0;
-//     BPHi=30;
-//     BPlo=0;
-//     Cp=pm25;
-//     pm25Index= ((IHi - Ilo) / (BPHi - BPlo))*(Cp - BPlo) + Ilo;
-//   }
-//   else if(pm25>=31 && pm<=60)
-//   {
-//     IHi=100;
-//     Ilo=51;
-//     BPHi=60;
-//     BPlo=31;
-//     Cp=pm25;
-//     pm25Index= ((IHi - Ilo) / (BPHi - BPlo))*(Cp - BPlo) + Ilo;
-//   }
-//   else if(pm25>=61 && pm<=90)
-//   {
-//     IHi=200;
-//     Ilo=101;
-//     BPHi=90;
-//     BPlo=61;
-//     Cp=pm25;
-//     pm25Index= ((IHi - Ilo) / (BPHi - BPlo))*(Cp - BPlo) + Ilo;
-//   }
-//   else if(pm25>=91 && pm<=120)
-//   {
-//     IHi=300;
-//     Ilo=201;
-//     BPHi=120;
-//     BPlo=91;
-//     Cp=pm25;
-//     pm25Index= ((IHi - Ilo) / (BPHi - BPlo))*(Cp - BPlo) + Ilo;
-//   }
-//   else if(pm25>=121 && pm<=250)
-//   {
-//     IHi=400;
-//     Ilo=301;
-//     BPHi=250;
-//     BPlo=121;
-//     Cp=pm25;
-//     pm25Index= ((IHi - Ilo) / (BPHi - BPlo))*(Cp - BPlo) + Ilo;
-//   }
-//   else
-//   {
-//     IHi=500;
-//     Ilo=401;
-//     BPHi=379;
-//     BPlo=250;
-//     Cp=pm25;
-//     pm25Index= ((IHi - Ilo) / (BPHi - BPlo))*(Cp - BPlo) + Ilo;
-//   }
-//   aqiv = Math.max(aqiv, pm25Index);
-
-//   // Carbon monoxide (CO) calculation
-
-//   if(co>=0 && co<=1)
-//   {
-//     IHi=50;
-//     Ilo=0;
-//     BPHi=1;
-//     BPlo=0;
-//     Cp=co;
-//     coIndex= ((IHi - Ilo) / (BPHi - BPlo))*(Cp - BPlo) + Ilo;
-//   }
-//   else if(co>=1.1 && co<=2)
-//   {
-//     IHi=100;
-//     Ilo=51;
-//     BPHi=2;
-//     BPlo=1.1;
-//     Cp=co;
-//     coIndex= ((IHi - Ilo) / (BPHi - BPlo))*(Cp - BPlo) + Ilo;
-//   }
-//   else if(co>=2.1 && co<=10)
-//   {
-//     IHi=200;
-//     Ilo=101;
-//     BPHi=10;
-//     BPlo=2.1;
-//     Cp=co;
-//     coIndex= ((IHi - Ilo) / (BPHi - BPlo))*(Cp - BPlo) + Ilo;
-//   }
-//   else if(co>=10.1 && co<=17)
-//   {
-//     IHi=300;
-//     Ilo=201;
-//     BPHi=17;
-//     BPlo=10.1;
-//     Cp=co;
-//     coIndex= ((IHi - Ilo) / (BPHi - BPlo))*(Cp - BPlo) + Ilo;
-//   }
-//   else if(co>=17.1 && co<=34)
-//   {
-//     IHi=400;
-//     Ilo=301;
-//     BPHi=34;
-//     BPlo=17.1;
-//     Cp=co;
-//     coIndex= ((IHi - Ilo) / (BPHi - BPlo))*(Cp - BPlo) + Ilo;
-//   }
-//   else
-//   {
-//     IHi=500;
-//     Ilo=401;
-//     BPHi=69;
-//     BPlo=34.1;
-//     Cp=co;
-//     coIndex= ((IHi - Ilo) / (BPHi - BPlo))*(Cp - BPlo) + Ilo;
-//   }
-//   aqiv = Math.max(aqiv, coIndex);
-
-//   return aqiv;
-// }
 function ozone(o3)
 {
   var IHi = null;
@@ -329,69 +147,69 @@ function ozone(o3)
   }
   return o3Index;
 }
-function pm(pm25)
+function pm(pm10)
 {
   var IHi = null;
   var Ilo = null;
   var BPHi = null;
   var BPlo = null;
   var Cp = null;
-  var pm25Index = null;
-  if(pm25>=0 && pm<=30)
+  var pm10Index = null;
+  if(pm10>=0 && pm10<=50)
   {
     IHi=50;
     Ilo=0;
-    BPHi=30;
+    BPHi=50;
     BPlo=0;
-    Cp=pm25;
-    pm25Index= ((IHi - Ilo) / (BPHi - BPlo))*(Cp - BPlo) + Ilo;
+    Cp=pm10;
+    pm10Index= ((IHi - Ilo) / (BPHi - BPlo))*(Cp - BPlo) + Ilo;
   }
-  else if(pm25>=31 && pm<=60)
+  else if(pm10>=51 && pm10<=100)
   {
     IHi=100;
     Ilo=51;
-    BPHi=60;
-    BPlo=31;
-    Cp=pm25;
-    pm25Index= ((IHi - Ilo) / (BPHi - BPlo))*(Cp - BPlo) + Ilo;
+    BPHi=100;
+    BPlo=51;
+    Cp=pm10;
+    pm10Index= ((IHi - Ilo) / (BPHi - BPlo))*(Cp - BPlo) + Ilo;
   }
-  else if(pm25>=61 && pm<=90)
+  else if(pm10>=101 && pm10<=250)
   {
     IHi=200;
     Ilo=101;
-    BPHi=90;
-    BPlo=61;
-    Cp=pm25;
-    pm25Index= ((IHi - Ilo) / (BPHi - BPlo))*(Cp - BPlo) + Ilo;
+    BPHi=250;
+    BPlo=101;
+    Cp=pm10;
+    pm10Index= ((IHi - Ilo) / (BPHi - BPlo))*(Cp - BPlo) + Ilo;
   }
-  else if(pm25>=91 && pm<=120)
+  else if(pm10>=251 && pm10<=350)
   {
     IHi=300;
     Ilo=201;
-    BPHi=120;
-    BPlo=91;
-    Cp=pm25;
-    pm25Index= ((IHi - Ilo) / (BPHi - BPlo))*(Cp - BPlo) + Ilo;
+    BPHi=350;
+    BPlo=251;
+    Cp=pm10;
+    pm10Index= ((IHi - Ilo) / (BPHi - BPlo))*(Cp - BPlo) + Ilo;
   }
-  else if(pm25>=121 && pm<=250)
+  else if(pm10>=351 && pm10<=430)
   {
     IHi=400;
     Ilo=301;
-    BPHi=250;
-    BPlo=121;
-    Cp=pm25;
-    pm25Index= ((IHi - Ilo) / (BPHi - BPlo))*(Cp - BPlo) + Ilo;
+    BPHi=430;
+    BPlo=351;
+    Cp=pm10;
+    pm10Index= ((IHi - Ilo) / (BPHi - BPlo))*(Cp - BPlo) + Ilo;
   }
   else
   {
     IHi=500;
     Ilo=401;
-    BPHi=379;
-    BPlo=250;
-    Cp=pm25;
-    pm25Index= ((IHi - Ilo) / (BPHi - BPlo))*(Cp - BPlo) + Ilo;
+    BPHi=510;
+    BPlo=431;
+    Cp=pm10;
+    pm10Index= ((IHi - Ilo) / (BPHi - BPlo))*(Cp - BPlo) + Ilo;
   }
-  return pm25Index;
+  return pm10Index;
 }
 function coc(co)
 {
@@ -464,7 +282,27 @@ button.addEventListener('click', function(name){
 
 try{
   a();
+  var descValue = null;
+  var tempValue = null;
+  var feelsValue = null;
+  var humValue = null;
+  var latValue = null;
+  var longValue = null;
+  var aqiValue = null;
+  var coValue = null;
+  var pmValue = null;
+  var o3Value = null;
   main.innerHTML = "Place not Found!";
+  desc.innerHTML = "Desc - "+descValue;
+  temp.innerHTML = "Temp - "+tempValue;
+  feels.innerHTML = "Feels Like - "+feelsValue;
+  hum.innerHTML = "Humidity - "+humValue;
+  lat.innerHTML = "Latitute - "+latValue;
+  long.innerHTML = "Longitude - "+longValue;
+  aqi.innerHTML = "AQI - "+aqiValue;
+  co.innerHTML = "CO - "+coValue;
+  pmI.innerHTML = "PM10 - "+pmValue;
+  o3I.innerHTML = "O3 - "+o3Value;
 }
 
 
